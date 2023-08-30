6e033f92e8113ba1a2ac39291d2acf5520cffd52
Ehsan Maiqani <spin313@gmail.com>
2023-08-30 13:08:52 +0330

    Basic functions added

---
+++ ppc-optimizer/ppc-optimizer.py
@@ -0,0 +1,18 @@
+
+class PpcOptimizer:
+    _data = None
+
+    def __init__(self, data):
+        self._data = data
+
+    def click_sale_criteria(self, click, sale, bid):
+        pass
+
+    def impression_ctr_sale_criteria(self, impression, ctr, sale, bid):
+        pass
+
+    def low_acos_criteria(self, acos, target):
+        pass
+
+    def high_acos_criteria(self, acos, target):
+        pass
