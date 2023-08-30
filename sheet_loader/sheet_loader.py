a550740412a2b359cb9bec197f6fb037dbc491a0
Ehsan Maiqani <spin313@gmail.com>
2023-08-30 13:26:02 +0330

    Reading functions completed

--- sheet_loader/sheet_loader.py
+++ sheet_loader/sheet_loader.py
@@ -3,9 +3,9 @@ import pandas
 
 class SheetLoader:
     _portfolios = None
-    _spcs = None
-    _sbcs = None
-    _sdcs = None
+    _sponsored_prod_camp = None
+    _sponsored_brand_camp = None
+    _sponsored_disp_camp = None
     _sp_search_term_report = None
 
     def __init__(self, filename):
@@ -19,4 +19,10 @@ class SheetLoader:
         self._portfolios = pandas.read_excel(filename, sheet_name='Portfolios')
 
     def read_sponsored_products_campaigns(self, filename):
-        self._spcs = pandas.read_excel(filename, sheet_name='Sponsored Products Campaigns')
+        self._sponsored_prod_camp = pandas.read_excel(filename, sheet_name='Sponsored Products Campaigns')
+
+    def read_sponsored_brands_campaigns(self, filename):
+        self._sponsored_brand_camp = pandas.read_excel(filename, sheet_name='Sponsored Brands Campaigns')
+
+    def read_sponsored_display_campaigns(self, filename):
+        self._sponsored_disp_camp = pandas.read_excel(filename, sheet_name='SP Search Term Report')
