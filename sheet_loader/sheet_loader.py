ec3615e0501827d0d26d5173117c1d2c008ec501
Ehsan Maiqani <spin313@gmail.com>
2023-08-30 14:01:10 +0330

    Returning data added

--- sheet_loader/sheet_loader.py
+++ sheet_loader/sheet_loader.py
@@ -8,7 +8,7 @@ class SheetLoader:
     _sponsored_disp_camp = None
     _sp_search_term_report = None
 
-    def __init__(self, filename):
+    def __init__(self):
         pass
 
     @property
@@ -17,12 +17,16 @@ class SheetLoader:
 
     def read_portfolios(self, filename):
         self._portfolios = pandas.read_excel(filename, sheet_name='Portfolios')
+        return self._portfolios
 
     def read_sponsored_products_campaigns(self, filename):
         self._sponsored_prod_camp = pandas.read_excel(filename, sheet_name='Sponsored Products Campaigns')
+        return self._sponsored_prod_camp
 
     def read_sponsored_brands_campaigns(self, filename):
         self._sponsored_brand_camp = pandas.read_excel(filename, sheet_name='Sponsored Brands Campaigns')
+        return self._sponsored_brand_camp
 
     def read_sponsored_display_campaigns(self, filename):
         self._sponsored_disp_camp = pandas.read_excel(filename, sheet_name='SP Search Term Report')
+        return self._sponsored_disp_camp
