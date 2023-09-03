106d55a7be6eac53d16e32103e5e5f1db854d5ca
Ehsan Maiqani <spin313@gmail.com>
2023-09-03 10:08:49 +0330

    low_impression_low_ctr_low_sale_rule implemented

--- Optimizer/apex_optimizer.py
+++ Optimizer/apex_optimizer.py
@@ -1,4 +1,3 @@
-
 USD_TO_AED_FACTOR = 3.67
 AED_TO_USD_FACTOR = 0.27
 
@@ -10,6 +9,7 @@ APEX_INCREASE_BID_FACTOR = 1.2
 APEX_DECREASE_BID_FACTOR = 0.8
 APEX_MIN_BID_VALUE = 0.2 * USD_TO_AED_FACTOR
 APEX_MAX_BID_VALUE = 2.0 * USD_TO_AED_FACTOR
+
 
 class ApexOptimizer:
     _data = None
@@ -26,12 +26,19 @@ class ApexOptimizer:
             item["Bid"] = APEX_MIN_BID_VALUE
             item["Operation"] = "update"
 
-            return item
-        return None
+        return item
 
     @staticmethod
-    def low_impression_low_ctr_low_sale_rule(item, impression, ctr, sale, bid):
-        pass
+    def low_impression_low_ctr_low_sale_rule(item):
+        impression = int(item["Impressions"])
+        ctr = float(item["Click-through Rate"])
+        orders = int(item["Orders"])
+
+        if impression >= APEX_IMPRESSION_NUM_THRESHOLD and ctr < APEX_LOW_CTR_THRESHOLD and orders == 0:
+            item["Bid"] = APEX_MIN_BID_VALUE
+            item["Operation"] = "update"
+
+        return item
 
     @staticmethod
     def profitable_acos_rule(item, acos, target):
