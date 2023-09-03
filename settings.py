a78a093ea316b3195cdf88048d4834a99d5e5814
Ehsan Maiqani <spin313@gmail.com>
2023-09-03 13:54:24 +0330

    Settings file added

---
+++ settings.py
@@ -0,0 +1,8 @@
+# Poor performer limits
+# Bad CTR definitions: Higher than 2500 impression and less than 0.15 # CTR / 0 order
+BAD_CTR_IMPRESSION_THRESHOLD = 2500     # Count
+BAD_CTR_CTR_THRESHOLD = 0.15            # Percent
+BAD_CTR_ORDERS_THRESHOLD = 0            # Count
+
+# Bid override setting
+ACOS_THRESHOLD = 35                     # Percent
