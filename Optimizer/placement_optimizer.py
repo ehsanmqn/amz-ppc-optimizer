a14a10de4b8f9e84991ff37f7f26b06be5a229e0
Ehsan Maiqani <spin313@gmail.com>
2023-09-19 11:08:12 +0330

    Bid+ optimizer method implemented

--- Optimizer/placement_optimizer.py
+++ Optimizer/placement_optimizer.py
@@ -1,10 +1,30 @@
-
 class PlacementOptimizer:
     """
     Placement Optimizer
     """
 
     _data_sheet = None
+
+    # Campaign bidding strategy
+    _campaign_bidding_strategies = {
+        # We’ll raise your bids (by a maximum of 100%) in real time when your ad may be more likely to convert to a
+        # sale, and lower your bids when less likely to convert to a sale.
+        "dynamic": "Dynamic bids - up and down",
+
+        # We’ll lower your bids in real time when your ad may be less likely to convert to a sale.
+        "dynamic_down": "Dynamic bids - down only",
+
+        # We’ll use your exact bid and any manual adjustments you set, and won’t change your bids based on likelihood
+        # of a sale.
+        "fixed": "Fixed bid"
+    }
+
+    # Adjust bids by placement (replaces Bid+)
+    # Example: A AED1.00 bid will remain AED1.00 for placement factor 0%. Dynamic bidding may increase it up to AED2.00.
+    _adjust_first_page_factor = 0  # Percentage
+
+    # Example: A AED1.00 bid will remain AED1.00 for placement factor 0%. Dynamic bidding may increase it up to AED1.50.
+    _adjust_product_page_factor = 0  # Percentage
 
     def __init__(self, data):
         self._data_sheet = data
@@ -31,5 +51,41 @@ class PlacementOptimizer:
         """
         return item["Entity"] == "Bidding Adjustment"
 
-    def optimize_campaign(self):
-        pass
+    @staticmethod
+    def is_campaign_enabled(item):
+        """
+        Check whether campaign is enabled
+        :param item:
+        :return:
+        """
+        return item["Campaign State (Informational only)"] == "enabled"
+
+    def bid_plus_optimize(self, campaigns, strategy, adjust_first_page_factor=None, adjust_product_page_factor=None):
+        """
+        Bid+ optimizer method
+        :return:
+        """
+
+        if adjust_first_page_factor is None:
+            adjust_first_page_factor = self._adjust_first_page_factor
+
+        if adjust_product_page_factor is None:
+            adjust_product_page_factor = self._adjust_product_page_factor
+
+        for index, row in self._data_sheet.iterrows():
+            # Adjust campaign
+            if self.is_campaign_enabled(row):
+                if self.is_campaign(row):
+                    if row["Campaign Name (Informational only)"] in campaigns:
+                        row["Bidding Strategy"] = self._campaign_bidding_strategies[strategy]
+                        row["Operation"] = "update"
+
+                # Adjust bidding adjustment
+                if self.is_bidding_adjustment(row):
+                    if row["Campaign Name (Informational only)"] in campaigns:
+                        if row["Placement"] == "Placement Top":
+                            row["Percentage"] = adjust_first_page_factor
+                            row["Operation"] = "update"
+                        elif row["Placement"] == "Placement Product Page":
+                            row["Percentage"] = adjust_product_page_factor
+                            row["Operation"] = "update"
