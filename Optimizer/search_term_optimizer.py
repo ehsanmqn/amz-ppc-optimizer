d5fd13616415f58207161d8c19473bd2c377163e
Ehsan Maiqani <spin313@gmail.com>
2023-09-23 10:54:28 +0330

    Search term optimizer added

---
+++ Optimizer/search_term_optimizer.py
@@ -0,0 +1,13 @@
+class SearchTermOptimizer:
+    """
+    Placement Optimizer
+    """
+
+    _data_sheet = None
+
+    def __init__(self, data):
+        self._data_sheet = data
+
+    @property
+    def datasheet(self):
+        return self._data_sheet
