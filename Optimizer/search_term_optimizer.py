55e2b043f230677d16c94eec068a95c0ef6f24e4
Ehsan Maiqani <spin313@gmail.com>
2023-09-23 13:28:54 +0330

    find_unprofitable_search_terms and find_profitable_search_terms implemented

--- Optimizer/search_term_optimizer.py
+++ Optimizer/search_term_optimizer.py
@@ -12,10 +12,32 @@ class SearchTermOptimizer:
     def datasheet(self):
         return self._data_sheet
 
-    def find_profitable_search_terms(self):
-        pass
+    def find_profitable_search_terms(self, desired_acos):
+        """
+        Return search terms that have ACOS lower than desired ACOS
+        :return:
+        """
 
-    def find_unprofitable_search_terms(self):
+        search_terms = self._data_sheet
+        result = search_terms[(search_terms["Total Advertising Cost of Sales (ACOS) "] < desired_acos) & (
+                search_terms["Total Advertising Cost of Sales (ACOS) "] > 0)]
+        result = result.sort_values(by=["Total Advertising Cost of Sales (ACOS) "], ascending=False)
+
+        return result
+
+    def find_unprofitable_search_terms(self, desired_acos):
+        """
+        Return search terms that have ACOS higher than desired ACOS
+        :param desired_acos:
+        :return:
+        """
+
+        search_terms = self._data_sheet
+        result = search_terms[(search_terms["Total Advertising Cost of Sales (ACOS) "] > desired_acos)]
+        result = result.sort_values(by=["Total Advertising Cost of Sales (ACOS) "], ascending=False)
+
+        return result
+
         pass
 
     def add_search_terms(self, search_terms, exact_match_campaign, phrase_match_campaign, broad_match_campaign):
--- main.py
+++ main.py
@@ -10,23 +10,27 @@ EXCLUDE_AD_GROUPS = []
 
 
 def main():
-    campaigns_bulk_sheet = AmzSheetHandler(filename="data.xlsx")
-    campaigns_bulk_sheet.read_data_file(sheet_type="campaigns")
+    # campaigns_bulk_sheet = AmzSheetHandler(filename="data.xlsx")
+    # campaigns_bulk_sheet.read_data_file(sheet_type="campaigns")
 
     # placement_optimizer = PlacementOptimizer(loader.sponsored_prod_camp)
     # profitable_orders = placement_optimizer.filter_campaigns_acos(threshold=.3)
     # print(profitable_orders["Campaign Name"])
 
-    keyword_optimizer = ApexOptimizer(campaigns_bulk_sheet.sponsored_prod_camp)
-    keyword_optimizer.optimize_keywords(exclude_dynamic_bids=True)
+    # keyword_optimizer = ApexOptimizer(campaigns_bulk_sheet.sponsored_prod_camp)
+    # keyword_optimizer.optimize_keywords(exclude_dynamic_bids=True)
 
     search_terms_sheet = AmzSheetHandler(filename="Sponsored Products Search term report.xlsx")
     search_terms_sheet.read_data_file(sheet_type="terms")
 
     search_terms_optimizer = SearchTermOptimizer(search_terms_sheet.sponsored_product_search_terms)
+    profitable_st = search_terms_optimizer.find_profitable_search_terms(desired_acos=0.3)
+    unprofitable_st = search_terms_optimizer.find_unprofitable_search_terms(desired_acos=0.3)
+    print(profitable_st["Total Advertising Cost of Sales (ACOS) "])
+    print(unprofitable_st["Total Advertising Cost of Sales (ACOS) "])
 
-    filename = "Sponsored Products Campaigns_" + str(datetime.datetime.utcnow().date()) + ".xlsx"
-    campaigns_bulk_sheet.write_data_file(filename, keyword_optimizer.datasheet, "Sponsored Products Campaigns")
+    # filename = "Sponsored Products Campaigns_" + str(datetime.datetime.utcnow().date()) + ".xlsx"
+    # campaigns_bulk_sheet.write_data_file(filename, keyword_optimizer.datasheet, "Sponsored Products Campaigns")
 
 
 if __name__ == "__main__":
