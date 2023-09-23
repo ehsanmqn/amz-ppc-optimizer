e217a0f29d23e395df6321a621cffb4314d11f79
Ehsan Maiqani <spin313@gmail.com>
2023-09-23 13:14:58 +0330

    find_profitable_search_terms and find_unprofitable_search_terms added

--- Optimizer/search_term_optimizer.py
+++ Optimizer/search_term_optimizer.py
@@ -12,5 +12,11 @@ class SearchTermOptimizer:
     def datasheet(self):
         return self._data_sheet
 
+    def find_profitable_search_terms(self):
+        pass
+
+    def find_unprofitable_search_terms(self):
+        pass
+
     def add_search_terms(self, exact_match_campaign, phrase_match_campaign, broad_match_campaign):
         pass
