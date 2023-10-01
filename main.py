202b10c6c3dc351a770b0e206bab5d38adf65667
Ehsan Maiqani <spin313@gmail.com>
2023-10-01 12:07:01 +0330

    Project structure reformed

--- main.py
+++ main.py
@@ -1,38 +1,10 @@
 import datetime
 
-import settings
 from amz_sheet_handler.amz_sheet_handler import AmzSheetHandler
+
 from core.apex_optimizer import ApexOptimizer
 from core.search_term_optimizer import SearchTermOptimizer
-
-EXCLUDE_AD_GROUPS = []
-
-
-def add_search_terms(datagram, search_terms, bid_factor):
-    # Add profitable search terms to exact campaigns
-    exact_camp_name = settings.DEFAULT_EXACT_ST_CAMPAIGN_NAME
-    if AmzSheetHandler.is_campaign_exists(datagram, exact_camp_name) is False:
-        datagram = AmzSheetHandler.add_campaign(datagram, exact_camp_name, exact_camp_name)
-
-    # Add profitable search terms to phrase campaigns
-    phrase_camp_name = settings.DEFAULT_PHRASE_ST_CAMPAIGN_NAME
-    if AmzSheetHandler.is_campaign_exists(datagram, phrase_camp_name) is False:
-        datagram = AmzSheetHandler.add_campaign(datagram, phrase_camp_name, phrase_camp_name)
-
-    # Add profitable search terms to broad campaigns
-    broad_camp_name = settings.DEFAULT_BROAD_ST_CAMPAIGN_NAME
-    if AmzSheetHandler.is_campaign_exists(datagram, broad_camp_name) is False:
-        datagram = AmzSheetHandler.add_campaign(datagram, broad_camp_name, broad_camp_name)
-
-    for index, row in search_terms.iterrows():
-        keyword = row["Customer Search Term"]
-        if AmzSheetHandler.is_keyword_exists(datagram, keyword, "Exact") is False:
-            bid = float(row["Cost Per Click (CPC)"])
-            datagram = AmzSheetHandler.add_keyword(datagram, exact_camp_name, exact_camp_name, keyword, bid * bid_factor, "Exact")
-            datagram = AmzSheetHandler.add_keyword(datagram, phrase_camp_name, phrase_camp_name, keyword, bid * bid_factor, "Phrase")
-            datagram = AmzSheetHandler.add_keyword(datagram, broad_camp_name, broad_camp_name, keyword, bid * bid_factor, "Broad")
-
-    return datagram
+from utils import add_search_terms
 
 
 def main():
