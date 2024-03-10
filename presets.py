963fdfe7cec44f51f47c682a384e27a6e405a88b
Ehsan Maiqani <spin313@gmail.com>
2024-03-10 11:25:13 +0330

    presets structure updated

--- presets.py
+++ presets.py
@@ -19,30 +19,8 @@ ae_product_portfolio = {
     }
 }
 
-ae_excluded_campaigns = [
-    "Loofah - Long2- Exact - 30",
-    "Loofah - Long8 - Exact - 30",
-    "Loofah - Long10 - Exact - 30",
-    "Loofah - Long3- Exact - 30",
-    "Loofah - Long11 - Exact - 30",
-    "Loofah - Long1- Exact - 30",
-    "Loofah - Long4- Exact - 30",
-    "Loofah - Long6- Exact - 30",
-    "Loofah - Long7 - Exact - 30",
-    "Loofah - Long9 - Exact - 30",
-    "Loofah - Long5- Exact - 30",
-    "CatchAll - Auto",
-    "CatchAll - ASIN - Manual",
-    "CatchAll - Keyword - Manual",
-    "L'Evesque - Brand Defense - ASIN",
-    "L'Evesque - Brand Defense - Phrase",
-    "HairBrush - Pink - Exact",
-    "HairBrush - Purple - Exact"
-]
 
-ae_excluded_portfolios = [
 
-]
 
 ae_presets = {
     "desired_acos": 0.3,        # x100 %
@@ -55,7 +33,28 @@ ae_presets = {
     "click_limit": 11,          # Count
     "impression_limit": 300,    # Count
     "step_up": 0.15,            # Currency
-    "low_impression_max_value": 1          # Currency
+    "low_impression_max_value": 1,          # Currency
+    "excluded_portfolios": [],
+    "excluded_campaigns": [
+        "Loofah - Long2- Exact - 30",
+        "Loofah - Long8 - Exact - 30",
+        "Loofah - Long10 - Exact - 30",
+        "Loofah - Long3- Exact - 30",
+        "Loofah - Long11 - Exact - 30",
+        "Loofah - Long1- Exact - 30",
+        "Loofah - Long4- Exact - 30",
+        "Loofah - Long6- Exact - 30",
+        "Loofah - Long7 - Exact - 30",
+        "Loofah - Long9 - Exact - 30",
+        "Loofah - Long5- Exact - 30",
+        "CatchAll - Auto",
+        "CatchAll - ASIN - Manual",
+        "CatchAll - Keyword - Manual",
+        "L'Evesque - Brand Defense - ASIN",
+        "L'Evesque - Brand Defense - Phrase",
+        "HairBrush - Pink - Exact",
+        "HairBrush - Purple - Exact"
+    ]
 }
 
 us_presets = {
@@ -69,15 +68,13 @@ us_presets = {
     "click_limit": 11,          # Count
     "impression_limit": 300,    # Count
     "step_up": 0.05,            # Currency
-    "low_impression_max_value": 0.35       # Currency
+    "low_impression_max_value": 0.35,       # Currency
+    "excluded_campaigns": [
+        "CatchAll - Auto",
+        "CatchAll - ASIN",
+        "CatchAll - Broad"
+    ],
+    "us_excluded_portfolios": [
+        "SuperBrush"
+    ]
 }
-
-us_excluded_campaigns = [
-    "CatchAll - Auto",
-    "CatchAll - ASIN",
-    "CatchAll - Broad"
-]
-
-us_excluded_portfolios = [
-    "SuperBrush"
-]
